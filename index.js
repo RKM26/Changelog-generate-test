38ecc6534ca829629890adb45169fed9a852dcbe
RKM26 <rishabhkumarmayank@gmail.com>
2023-02-13 16:43:15 +0530

    dev branch 1 commit

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var a = 9;
-var c = 8;
-console.log('develop added again');
-console.log('perf added again');
-console.log('perf again');
-console.log('fix again');
-console.log('feat again');
-console.log('fix 1');
-console.log('dev branch 1');
+console.log('dev branch 1 commit');
